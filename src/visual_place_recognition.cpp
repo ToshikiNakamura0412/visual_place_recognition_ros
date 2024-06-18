95e85d85eb33ca1f1fe0b3e41af79b168047ef84
Toshiki Nakamura <tnakamura.amsl@gmail.com>
2024-06-19 03:55:39 +0900

    Add evaluation

--- src/visual_place_recognition.cpp
+++ src/visual_place_recognition.cpp
@@ -12,8 +12,10 @@
 
 VPR::VPR(void) : private_nh_("~")
 {
+  private_nh_.param<std::string>("mode", mode_, std::string("runtime"));  // or "evaluation"
   private_nh_.param<std::string>("voc_file_path", voc_file_path_, std::string(""));
-  private_nh_.param<std::string>("image_dir_path", image_dir_path_, std::string(""));
+  private_nh_.param<std::string>("ref_image_dir_path", ref_image_dir_path_, std::string(""));
+  private_nh_.param<std::string>("query_image_dir_path", query_image_dir_path_, std::string(""));
   private_nh_.param<float>("match_threshold", match_threshold_, 0.9);
   private_nh_.param<int>("resolution", resolution_, 240);
 
@@ -22,10 +24,12 @@ VPR::VPR(void) : private_nh_("~")
   image_pub_ = nh_.advertise<sensor_msgs::Image>("custom_image", 1);
 
   ROS_INFO_STREAM(ros::this_node::getName() << " node has started..");
-  ROS_INFO_STREAM("voc_file_path: " << voc_file_path_);
-  ROS_INFO_STREAM("image_dir_path: " << image_dir_path_);
-  ROS_INFO_STREAM("match_threshold: " << match_threshold_);
-  ROS_INFO_STREAM("resolution: " << resolution_);
+  ROS_INFO_STREAM("  mode: " << mode_);
+  ROS_INFO_STREAM("  voc_file_path: " << voc_file_path_);
+  ROS_INFO_STREAM("  ref_image_dir_path: " << ref_image_dir_path_);
+  ROS_INFO_STREAM("  query_image_dir_path: " << query_image_dir_path_);
+  ROS_INFO_STREAM("  match_threshold: " << match_threshold_);
+  ROS_INFO_STREAM("  resolution: " << resolution_);
   ROS_INFO_STREAM("");
 }
 
@@ -41,7 +45,7 @@ void VPR::image_callback(const sensor_msgs::ImageConstPtr &msg)
   }
   image_pub_.publish(cv_ptr->toImageMsg());
   scale_to_resolution(cv_ptr->image, resolution_);
-  query2(calc_features(cv_ptr->image));
+  query_pose(calc_features(cv_ptr->image));
 }
 
 void VPR::scale_to_resolution(cv::Mat &image, const int resolution)
@@ -60,7 +64,7 @@ cv::Mat VPR::calc_features(const cv::Mat &image)
   return descriptors;
 }
 
-void VPR::query2(const cv::Mat &features)
+void VPR::query_pose(const cv::Mat &features)
 {
   DBoW3::QueryResults ret;
   db_.query(features, ret, 4);
@@ -90,15 +94,46 @@ void VPR::process(void)
 {
   const DBoW3::Vocabulary voc = load_vocabulary(voc_file_path_);
   db_ = create_database(voc);
-  // ROS_INFO_STREAM("Database info: " << db);
 
-  std::vector<std::string> image_file_paths = load_image_file_paths(image_dir_path_);
+  std::vector<std::string> ref_image_file_paths = load_image_file_paths(ref_image_dir_path_);
+  const std::vector<cv::Mat> features = load_features(ref_image_file_paths);
+
+  load_poses(ref_image_dir_path_);
+  add_db(features, db_);
+}
+
+void VPR::create_confusion_matrix(void)
+{
+  const DBoW3::Vocabulary voc = load_vocabulary(voc_file_path_);
+  db_ = create_database(voc);
+  std::vector<std::string> image_file_paths = load_image_file_paths(ref_image_dir_path_);
   const std::vector<cv::Mat> features = load_features(image_file_paths);
-
-  load_poses(image_dir_path_);
   add_db(features, db_);
 
-  // query(db, features);
+  std::vector<std::string> image_file_paths2 = load_image_file_paths(query_image_dir_path_);
+  const std::vector<cv::Mat> features2 = load_features(image_file_paths2);
+  create_query_result(features2);
+}
+
+void VPR::create_query_result(const std::vector<cv::Mat> &features)
+{
+  ROS_INFO_STREAM("Querying..");
+  DBoW3::QueryResults ret;
+  std::string file_name = "result.csv";
+  std::ofstream ofs(file_name);
+  ofs << "query_image,ref0,ref1,ref2,ref3" << std::endl;
+  for (int i = 0; i < features.size(); i++)
+  {
+    db_.query(features[i], ret, 4);
+    if (ret[0].Score < match_threshold_)
+      continue;
+    ofs << i << ",";
+    for (const auto &r : ret)
+      ofs << r.Id << ",";
+    ofs << std::endl;
+  }
+  ROS_INFO_STREAM("Done");
+  ROS_INFO_STREAM("Result is saved as ~/.ros/" << file_name);
 }
 
 void VPR::add_db(const std::vector<cv::Mat> &features, DBoW3::Database &db)
@@ -107,7 +142,8 @@ void VPR::add_db(const std::vector<cv::Mat> &features, DBoW3::Database &db)
   for (int i = 0; i < features.size(); i++)
   {
     DBoW3::EntryId id = db.add(features[i]);
-    vpr_db_[i].id = id;
+    if (vpr_db_.size() > i)
+      vpr_db_[i].id = id;
   }
   ROS_INFO_STREAM("Done");
 }
@@ -188,13 +224,11 @@ std::vector<cv::Mat> VPR::load_features(const std::vector<std::string> &image_fi
 
   for (const auto &image_file_path : image_file_paths)
   {
-    // ROS_INFO_STREAM("Read image: " << image_file_path);
     cv::Mat image = cv::imread(image_file_path, 0);
 
     if (image.empty())
       throw std::runtime_error("Could not open image " + image_file_path);
 
-    // ROS_INFO_STREAM("Extract features: " << image_file_path);
     cv::Mat descriptors;
     fdetector->detectAndCompute(image, cv::Mat(), keypoints, descriptors);
     features.push_back(descriptors);
@@ -203,29 +237,23 @@ std::vector<cv::Mat> VPR::load_features(const std::vector<std::string> &image_fi
   return features;
 }
 
-void VPR::query(const DBoW3::Database &db, const std::vector<cv::Mat> &features)
-{
-  ROS_INFO_STREAM("Querying..");
-  ROS_INFO("----------------------------");
-  DBoW3::QueryResults ret;
-  for (int i = 0; i < features.size(); i++)
-  {
-    ROS_INFO_STREAM("Querying image " << i);
-    db.query(features[i], ret, 4);
-    for (const auto &r : ret)
-      if (r.Score > match_threshold_)
-        ROS_INFO_STREAM("Image " << r.Id << " is " << r.Score);
-    ROS_INFO("----------------------------");
-  }
-  ROS_INFO_STREAM("Done");
-}
-
 int main(int argc, char *argv[])
 {
   ros::init(argc, argv, "visual_place_recognition");
-  VPR visual_place_recognition;
-  visual_place_recognition.process();
-  ros::spin();
+  VPR vpr;
+  if (vpr.get_mode() == "runtime")
+  {
+    vpr.process();
+    ros::spin();
+  }
+  else if (vpr.get_mode() == "evaluation")
+  {
+    vpr.create_confusion_matrix();
+  }
+  else
+  {
+    ROS_ERROR_STREAM("Invalid mode");
+  }
 
   return 0;
 }
